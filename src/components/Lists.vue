e753f8eec287a96093e1a97aa1d0f009d75cfcce
jmartindg09 <jmartindeguia09@gmail.com>
2024-01-09 18:24:43 +0800

    added check outline icon when task is not done and added auto animate

--- src/components/Lists.vue
+++ src/components/Lists.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="">
-    <ul class="space-y-3">
+    <ul class="space-y-3" v-auto-animate>
       <li
         v-for="(todo, index) in todos"
         class="bg-white p-5 rounded-md flex justify-between items-center capitalize"
@@ -10,7 +10,7 @@
           >{{ todo.text }}</span
         >
         <div class="flex items-center space-x-2">
-          <button @click="todoStatus(index)">
+          <button @click="todoStatus(index)" v-if="todo.done">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -21,6 +21,22 @@
                 fill-rule="evenodd"
                 d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
                 clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+          <button @click="todoStatus(index)" v-else>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
               />
             </svg>
           </button>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { setupPinia } from "./piniaSetup";
+import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
 
 const app = createApp(App);
 
 setupPinia(app);
+
+app.use(autoAnimatePlugin);
 
 app.mount("#app");
